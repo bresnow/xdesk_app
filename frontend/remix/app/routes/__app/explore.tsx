53f8c8d7aeb714f77fc590d84b0422cbb010ee14
bresnow <bresnow@fltngmmth.com>
2022-12-29 03:31:29 +0000

    FRONTSPLIT
    Signed-off-by: bresnow <bresnow@fltngmmth.com>

---
+++ frontend/remix/app/routes/__app/explore.tsx
@@ -0,0 +1,40 @@
+import { Outlet } from "@remix-run/react";
+import type { LoaderFunction, MetaFunction } from "@remix-run/server-runtime";
+import type { LoaderContext } from "../../../types/index";
+import NaturalHome from "@ui/kit/templates/homePages/simplePage/NaturalHome";
+import TailKitHome from "@ui/kit/templates/homePages/simplePage/TailkitHome";
+import SimpleProjectDashboard from "../../../../ui/kit/templates/dashboardPages/projectDashboard/SimpleProjectDashboard";
+export const meta: MetaFunction = () => ({
+  title: "Explore",
+});
+export let loader: LoaderFunction = async ({ params, request, context }) => {
+  let loadCtx = context as unknown as LoaderContext;
+  return {
+    header: {
+      title: "browse",
+      subtitle: "Explore the Remix ecosystem",
+    },
+  };
+};
+export const handle = {
+  header: {
+    title: "Explore",
+  },
+};
+
+export default function ExploreRoute () {
+  return (
+    <div>
+      <Outlet />
+      <TailKitHome />
+      {/* <SimpleProjectDashboard/> */}
+      {/* //   <NaturalHome />
+    //   <MultipleShoppingCard /> */}
+      {/* <Trigger>
+    //             <ButtonFollow />
+    //         </Trigger>
+    //         <ButtonFollow action="/forms/user" />
+    //         <Outlet /> */}
+    </div>
+  );
+}
